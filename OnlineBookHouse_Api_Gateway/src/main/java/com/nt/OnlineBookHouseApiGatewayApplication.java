5b471c2331e92364904a7f011e02c6894db6bbf8
Satyam Kumar <your-email@example.com>
2026-05-21 12:23:49 +0530

    api gateway commit

---
+++ OnlineBookHouse_Api_Gateway/src/main/java/com/nt/OnlineBookHouseApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookHouseApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookHouseApiGatewayApplication.class, args);
+	}
+
+}
